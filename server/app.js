b9ea8a9ad9bf986bcf8312369c129aeb4f1cc02e
Trevor Puccini <trevorpuccini21@gmail.com>
2020-12-30 09:58:31 -0800

    Set up express server w/ graphql, remove REST endpoints

--- server/app.js
+++ server/app.js
@@ -5,53 +5,21 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dateFormat = require("dateformat");
 dotenv.config();
+const port = process.env.PORT;
+
+const { graphqlHTTP } = require("express-graphql");
 
 app.use(bodyParser.json());
 app.use(cors());
-const port = process.env.PORT;
 
-const problems = [
-  {
-    name: "Two Sum",
-    dates: [
-      ["12/14/20", false],
-      ["12/16/20", true],
-    ],
-    difficulty: "Easy",
-    url: "https://leetcode.com/problems/two-sum/",
-    id: 1,
-  },
-  {
-    name: "Rotate Image",
-    dates: [
-      ["12/18/20", false],
-      ["12/20/20", true],
-    ],
-    difficulty: "Medium",
-    url: "https://leetcode.com/problems/rotate-image/",
-    id: 48,
-  },
-];
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
-});
-
-// Set up get route, which returns an array of problem objects
-app.get("/", (req, res) => {
-  res.send(problems).status(200);
-});
-
-app.post("/", (req, res) => {
-  const body = req.body;
-  const dateAdded = dateFormat(new Date(), "shortDate");
-
-  const newProblem = {
-    ...body,
-    dates: [[dateAdded, false]],
-  };
-
-  console.log(newProblem);
-  problems.push(newProblem);
-  res.send(problems).status(201);
 });
