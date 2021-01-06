6a06ad5d547899ad8b4c7b8b459694cd8b2f05d0
Trevor Puccini <trevorpuccini21@gmail.com>
2021-01-05 17:11:25 -0800

    Add createProblem, retrieveProblems, and retrieveUser controllers

---
+++ server/controllers/problem.js
@@ -0,0 +1,29 @@
+const Problem = require("../models").Problem;
+
+const createProblem = (req, res) => {
+  return Problem.create({
+    name: "Two Sum",
+    url: "this",
+    difficulty: "Easy",
+    leetcodeId: 1,
+    userId: 1,
+  })
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
+
+const retrieveProblems = (userId) => {
+  return Problem.findAll({
+    where: {
+      userId: userId,
+    },
+    raw: true,
+  })
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
+
+module.exports = {
+  createProblem,
+  retrieveProblems,
+};
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -14,3 +14,20 @@ const createUser = (req, res) => {
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
+
+// retrieve a user by id
+const retrieveUser = (id) => {
+  return User.findOne({
+    where: {
+      id: id,
+    },
+    raw: true,
+  })
+    .then((res) => res)
+    .catch((err) => console.error(err));
+};
+
+module.exports = {
+  createUser,
+  retrieveUser,
+};
